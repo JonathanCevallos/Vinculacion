ea3326583ed7eb1c6b5f3f1f2e32c91141a2346b
Jonathan Cevallos <Jonathancevv@gmail.com>
2023-02-23 22:09:01 -0500

    commit

--- src/main/java/com/uteq/vinculacion/controller/HomeConroller.java
+++ src/main/java/com/uteq/vinculacion/controller/HomeConroller.java
@@ -1,2 +1,17 @@
-package com.uteq.vinculacion.controller;public class HomeConroller {
+package com.uteq.vinculacion.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeConroller {
+    @GetMapping({"/","/home", "/index"})
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/powerpoint")
+    public String powerpoint(){
+        return "powerpoint";
+    }
 }
