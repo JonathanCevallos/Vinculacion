9c72b7923afb1a833c73436bf95d84dfa1429819
eguarangao <69706207+eguarangao@users.noreply.github.com>
2023-02-23 23:18:00 -0500

    Sistema de rutas y paginas add

--- src/main/java/com/uteq/vinculacion/controller/HomeConroller.java
+++ src/main/java/com/uteq/vinculacion/controller/HomeConroller.java
@@ -5,13 +5,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeConroller {
-    @GetMapping({"/","/home", "/index"})
-    public String index(){
+    @GetMapping({"/", "/home", "/index"})
+    public String index() {
         return "index";
     }
 
     @GetMapping("/powerpoint")
-    public String powerpoint(){
+    public String powerpoint() {
         return "powerpoint";
+    }
+
+    @GetMapping("/word")
+    public String word() {
+        return "word";
+    }
+
+    @GetMapping("/pdf")
+    public String pdf() {
+        return "pdf";
+    }
+
+    @GetMapping("/audiolibros")
+    public String audiolibros() {
+        return "audiolibros";
     }
 }
